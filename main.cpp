9c83decfe86b482d00b653d24bafc6a1350dae7f
Aditya Ranjan <adityaranjan25@gmail.com>
2023-02-16 23:19:29 +0530

    Fixed stack tracing issue

--- main.cpp
+++ main.cpp
@@ -6,7 +6,10 @@ extern "C" {
 }
 /* Add all extern variables here */
 
+extern int yydebug;
+
 int main(int argc, char *argv[]) {
+    yydebug = 1;
     std::string exec_name = argv[0];
     std::string usage = "Usage: " + exec_name + " < (input_file) > (output_file)\n";
     yyparse();
